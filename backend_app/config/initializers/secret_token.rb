f64ab03af273fab5ae42eb5eae95e696591c61cb
johnnyjack <jp.agalves@gmail.com>
2014-02-21 17:50:23 +0000

    Prototype v1.0
    - Removed unnecessary files
    - Added backend files (NelsonSachse)

---
+++ backend_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackendApp::Application.config.secret_key_base = '063634b606c4a0e9b30741e2de3d4be4fad5f97c88d6fefb3674960bd87077771ba54e70546c92bc8855e211fefdc62271c6cf743d71e255ae9ff4e31681a7c8'
